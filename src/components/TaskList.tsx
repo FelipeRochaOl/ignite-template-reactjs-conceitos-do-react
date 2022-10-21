c4afda1cb016838ff28ab969a4c7b1c6c6e74c51
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-10-21 02:02:15 -0300

    Challenge 01 completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,35 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return
+    }
+    let countTasks = tasks.length;
+    const newTask: Task = {
+      id: countTasks++,
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const task = tasks.find(task => task.id === id)
+    if (!task) {
+      return
+    }
+    task as Task;
+    task.isComplete = !task.isComplete;
+    setTasks([...tasks])
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const reduceTasks = tasks.reduce((previous: Task[], current) => {             
+      if (current.id !== id) previous.push(current);
+      return previous;
+    }, []);
+    setTasks(reduceTasks)
   }
 
   return (
